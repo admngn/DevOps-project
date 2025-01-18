ab7b8a3af3316e6dd9274c1d29c30ade21473cb7
Adame Nianghane <adamenianghane41@gmail.com>
2025-01-18 22:35:18 +0100

    modif

--- test.py
+++ test.py
@@ -1,17 +1,12 @@
 import unittest
-from health_utils import calculate_bmi, calculate_bmr
+from health_utils import calculate_bmr
 
 class TestHealthUtils(unittest.TestCase):
-    def test_calculate_bmi(self):
-        self.assertAlmostEqual(calculate_bmi(1.75, 70), 22.86, places=2)
-
     def test_calculate_bmr_male(self):
-       
-        self.assertAlmostEqual(calculate_bmr(175, 70, 25, 'male'), 1668.1, places=1)
+        self.assertAlmostEqual(calculate_bmr(175, 70, 25, 'male'), 1724.05, places=1)
 
     def test_calculate_bmr_female(self):
-       
-        self.assertAlmostEqual(calculate_bmr(175, 70, 25, 'female'), 1496.0, places=1)
+        self.assertAlmostEqual(calculate_bmr(175, 70, 25, 'female'), 1528.78, places=1)
 
 if __name__ == '__main__':
     unittest.main()
